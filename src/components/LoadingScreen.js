492a02c210bfea8251076161586e9b43ad4dd14a
Marpace <almonacid.pablo@hotmail.com>
2022-10-21 10:52:54 -0400

    add readme and changed loading screen

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,8 +1,12 @@
 function LoadingScreen() {
   return (
     <div className="loading-screen">
-      <div className="spinner">
-        <div className="spinner-inner"></div>
+      <div className="loading-screen__icon">
+        <img className="palette-icon" src="./images/loading_palette.png"></img>
+        <div className="paint-circle"></div>
+        <div className="paint-circle"></div>
+        <div className="paint-circle"></div>
+        <div className="paint-circle"></div>
       </div>
     </div>
   )
